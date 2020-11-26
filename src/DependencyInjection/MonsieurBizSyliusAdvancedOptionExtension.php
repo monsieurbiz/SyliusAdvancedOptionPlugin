17a6d65938bdb1fc2c707e5ce587509ca3000577
Jacques Bodin-Hullin <j.bodinhullin@monsieurbiz.com>
2020-11-26 16:04:39 +0100

    Initialize the plugin with test Application and basic code

---
+++ src/DependencyInjection/MonsieurBizSyliusAdvancedOptionExtension.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of Monsieur Biz' Advanced Option plugin for Sylius.
+ *
+ * (c) Monsieur Biz <sylius@monsieurbiz.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MonsieurBiz\SyliusAdvancedOptionPlugin\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+final class MonsieurBizSyliusAdvancedOptionExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container): void
+    {
+        $this->processConfiguration($this->getConfiguration([], $container), $config);
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yaml');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAlias()
+    {
+        return str_replace('monsieur_biz', 'monsieurbiz', parent::getAlias());
+    }
+}
