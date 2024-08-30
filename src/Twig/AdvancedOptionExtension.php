eafbceb1762bf62c72472ae885be9570396ec0c5
Maxime Huran <m.huran@monsieurbiz.com>
2024-08-30 11:06:36 +0200

    Fix tests

--- src/Twig/AdvancedOptionExtension.php
+++ src/Twig/AdvancedOptionExtension.php
@@ -107,13 +107,15 @@ final class AdvancedOptionExtension extends AbstractExtension
     /**
      * @throws LoaderError
      * @throws SyntaxError
+     *
+     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
      */
     private function renderDefaultFormRow(FormView $optionForm): string
     {
         $formRowFunction = $this->twigEnvironment->getFunction('form_row');
-        if (false !== $formRowFunction) {
+        if (false !== $formRowFunction && null !== $formRowFunction) {
             $formRow = $formRowFunction->getCallable();
-            if (null !== $formRow) {
+            if (null !== $formRow && \is_callable($formRow)) {
                 return $this->twigEnvironment
                     ->createTemplate(
                         $formRow(
